8b5d049a58b11b2d18959ac9e9f066369b13f02d
Ashik <projectashik@gmail.com>
2022-08-28 18:13:54 +0000

    refactored

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,13 +1,5 @@
 import chalk from "chalk"
-import loading from "loading-cli"
-import {existsSync} from 'fs'
-import { print } from "../utils"
-import util from 'node:util'
-import child from 'node:child_process'
-
-
-const exec = util.promisify(child.exec);
-
+import { add, appendToFile, execute, print } from "../utils"
 
 const stylesContent = `
   @tailwind base;
@@ -16,20 +8,9 @@ const stylesContent = `
 `;
 
 export const addTailwind = async () => {
-    let pm = "npm"
-  if(existsSync("./yarn.lock")){
-    pm = "yarn"
-  }
-  let px = pm === "npm"? "npx" : "yarn"
-
-  print(`${chalk.green("Adding tailwind css to your project")}`)
-  await exec(`${pm} add tailwindcss postcss autoprefixer -D`)
-
-  print(`${chalk.green("Creating tailwind.config.js")}`)
-  await exec(`${px} tailwindcss init -p`)
-
-  // append stylesContent in styles/global.css
-await exec(`echo "${stylesContent}" >> ./styles/globals.css`)
+  await add(`tailwindcss postcss autoprefixer -D`)
+  await execute(`tailwindcss init -p`)
   print(chalk.green("Tailwind implemented successfully"));
+  await appendToFile(`./styles/globals.css`, stylesContent);
   process.exit(1)
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,51 @@
+import { existsSync } from "fs"
+
+import util from 'node:util'
+import child from 'node:child_process'
+import chalk from "chalk";
+import loading from "loading-cli";
+
+
+export const exec = util.promisify(child.exec);
+
+
+
 export const print = (args : any) => {
     console.log(args)
+}
+
+export const getPM = () => {
+    let pm = "npm"
+  if(existsSync("./yarn.lock")){
+    pm = "yarn"
+  }
+  let px = pm === "npm"? "npx" : "yarn"
+  return   {pm, px}
+}
+
+export const add = async (cmd: string) => {
+    const {pm} = getPM();
+    let pmCmd;
+    if(pm === "npm") {
+        pmCmd = "npm install"
+    } else {
+        pmCmd = "yarn add"
+    }
+    const load = loading(`${chalk.green("Executing")} ${chalk.yellow(`${pmCmd} ${cmd}`)}`)
+    load.start();
+    const { stderr, stdout } = await exec(`${pmCmd} ${cmd}`);
+    load.stop();
+    return {stderr, stdout}
+}
+
+export const execute = async (cmd: string) => {
+    const {px} = getPM();
+    const load = loading(`${chalk.green("Executing")} ${chalk.yellow(`${px} ${cmd}`)}`)
+    const { stderr, stdout } = await exec(`${px} ${cmd}`);
+    load.stop();
+    return {stderr, stdout}
+}
+
+export const appendToFile = async (file: string, content: any) => {
+    await exec(`echo "${content}" >> ${file}`)
 }
